88720dcd909a6d639fd8bf8f66c26ea071620877
Sathvikramprasad <sxr3949@mavs.uta.edu>
2024-09-09 16:34:29 -0500

    added Media.java

---
+++ P03/Media.java
@@ -0,0 +1,13 @@
+public class Media{
+    String title;
+    String url;
+
+    public Media(String title, String url){
+        this.title=title;
+        this.url=url;
+    }
+    @Override
+    public String toString(){
+        return title + " (" + url + ")";
+    }
+}
