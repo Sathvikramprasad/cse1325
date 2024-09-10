7667a542bfb7c6730f9475ca46a802df859eac21
Sathvikramprasad <sxr3949@mavs.uta.edu>
2024-09-09 22:39:08 -0500

    added url validation to media.java

--- Media.java
+++ Media.java
@@ -1,8 +1,18 @@
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class Media{
     String title;
     String url;
 
     public Media(String title, String url){
+       try {
+        URL urlTest = new URL(url);
+           
+       } catch (MalformedURLException e) {
+        throw new RuntimeException("Wrong URL");
+       }
+    
         this.title=title;
         this.url=url;
     }
