dc854bc589f8f3f316d7d29643dd1c30542e6ba6
Sathvikramprasad <sxr3949@mavs.uta.edu>
2024-09-09 21:08:05 -0500

    Account.java upload

---
+++ P03/Account.java
@@ -0,0 +1,16 @@
+public class Account{
+     int accountNumber;
+     static int nextAccountNumber = 1;
+    public Account(){
+        accountNumber = nextAccountNumber;
+        nextAccountNumber++;
+    }
+    public int getAccountNumber(){
+        return this.accountNumber;
+    }
+    public String play(Media media){
+        return "Playing "+media.toString();
+    }
+
+
+}
