b6b565fdb703df51fc8de7dc2574d840c5cde9ef
Sathvikramprasad <sxr3949@mavs.uta.edu>
2024-09-17 00:54:16 -0500

    added javadoc for Account.java

--- P04/customer/Account.java
+++ P04/customer/Account.java
@@ -1,15 +1,41 @@
 package customer;
 import product.Media;
+/**
+* an abstract class representing a general account for accessing media.
+*
+* @author     Sathvik Ramprasad
+* @version    0.2
+* @since      0.1
+* @license.agreement Gnu General Public License 3.0
+*/
 public abstract class Account{
      int accountNumber;
      static int nextAccountNumber = 1;
+     /**
+* initializes a new account with a new account number. The account number is incremented for each new Account created.
+*
+* @since    0.1
+*/
     public Account(){
         accountNumber = nextAccountNumber;
         nextAccountNumber++;
     }
+    /**
+ * returns the unique account number associated with this account.
+ * 
+ * @return    returns the account number.
+ * @since     0.1
+ */
     public int getAccountNumber(){
         return this.accountNumber;
     }
+    /**
+ * an abstract method to be implemented by subclasses to play the specified media.
+ * 
+ * @param    media the media object to be played.
+ * @return    returns a message indicating the result of the media play request.
+ * @since    0.2
+ */
     public abstract String play(Media media);
 
 
