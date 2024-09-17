a49e8fe0b5369817a12505cee2c41c2afee8ec7d
Sathvikramprasad <sxr3949@mavs.uta.edu>
2024-09-16 23:21:48 -0500

    added playMedia

--- P04/moes/Moes.java
+++ P04/moes/Moes.java
@@ -1,5 +1,8 @@
 package moes;
+import customer.Account;
+import customer.Alacarte;
 import customer.Student;
+import customer.Unlimited;
 import java.util.ArrayList;
 import product.Media;
 
@@ -29,6 +32,38 @@ public class Moes{
             i++;
         }
         return output;       
-    }   
-    
+    } 
+     public int getPoints(int num){
+        Student student = customers.get(num);
+        Account account = student.getAccount();
+        if (account instanceof Alacarte){
+            Alacarte a = (Alacarte) account;
+            int p1 = a.getPointsRemaining();
+            return p1;
+        } else if (account instanceof Unlimited) {
+            return  Integer.MAX_VALUE;
+        } else{
+            throw new UnsupportedOperationException("Unknown subclass of Account");
+        }
+    }
+      public String buyPoints(int num, int points){
+        Student student = customers.get(num);
+        Account account = student.getAccount();
+        if (account instanceof Alacarte){
+            Alacarte a = (Alacarte) account;
+            a.buyPoints(points);
+            int p1 = a.getPointsRemaining();
+            return "Available: " + p1;
+            
+        } else if (account instanceof Unlimited) {
+            return "Account type Unlimited, no need for points. ";
+        } else{
+            throw new UnsupportedOperationException("Unknown subclass of Account");
+        }
+    }
+    public String playMedia(int studentNumber, int mediaNumber){
+        Student student = customers.get(studentNumber);
+        Media media = library.get(mediaNumber);        
+        return student.requestMedia(media);
+    } 
 }
