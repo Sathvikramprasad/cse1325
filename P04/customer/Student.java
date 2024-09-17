1bafb8dd460f910ec2532add9434b0ada091a1e3
Sathvikramprasad <sxr3949@mavs.uta.edu>
2024-09-17 00:44:29 -0500

    added javadoc to Student.java

--- P04/customer/Student.java
+++ P04/customer/Student.java
@@ -1,10 +1,26 @@
 package customer;
 import product.Media;
+/**
+ * the Student class represents a UTA student with their media account, granting media access and management.
+ * 
+ * @author      Sathvik Ramprasad
+ * @version     0.2
+ * @since       0.1
+ */
 public class Student{
     String name;
     int id;
     String email;
     Account account;
+    /**
+ * constructs a Student object with their name, ID, email, and account type.
+ * 
+ * @param      name the student's name.
+ * @param      id the student's ID number.
+ * @param      email the student's email address.
+ * @param      unlimited a boolean indicating if the account type is Unlimited (true) or Alacarte (false).
+ * @since      0.2
+ */
 
    public Student(String name, int id, String email, boolean unlimited){
         if(!(email.endsWith("uta.edu"))){
@@ -20,6 +36,13 @@ public class Student{
             account = new Alacarte();
         }
     }
+    /**
+ * returns the account linked with the student.
+ * 
+ * @param     media the Media object that the student wants to play.
+ * @return    returns the Account object linked with the student.
+ * @since     0.2
+ */
 
    public String requestMedia(Media media){
         return account.play(media);
