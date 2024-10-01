141b1721cab0eb3f372a588a2158fbc73dfd9a6d
Sathvikramprasad <sxr3949@mavs.uta.edu>
2024-09-30 21:37:47 -0500

    completed until listMedia

---
+++ P05/mdi/Main.java
@@ -0,0 +1,12 @@
+package mdi;
+import moes.Moes;
+
+
+public class Main{
+    private Moes moes = new Moes();
+    private String output;
+    private Menu menu = new Menu();
+    private boolean running;
+
+}
+
