76744fa6654a119cca37227c6d390240c69fb7a7
Sathvikramprasad <sxr3949@mavs.uta.edu>
2024-11-11 22:32:37 -0600

    corrected few mistakes and created makefile

--- P10/full_credit/bank.cpp
+++ P10/full_credit/bank.cpp
@@ -6,26 +6,27 @@
 int main() {
     std::map<std::string, Purse> vault;
     int numAccounts;
-    std::cout << "How many accounts would you like to create? ";
+    std::cout << "How many accounts? ";
     std::cin >> numAccounts;
     std::cin.ignore();
 
     for (int i = 0; i < numAccounts; ++i) {
         std::string name;
         int pounds, shillings, pence;
-        std::cout << "Enter the name of account #" << (i + 1) << ": ";
+        std::cout << "Name account " << (i) << ": ";
         std::getline(std::cin, name);
-        std::cout << "Enter initial deposit for " << name << " (pounds shillings pence): ";
+        std::cout << "Enter initial deposit for (pounds shillings pence): ";
         std::cin >> pounds >> shillings >> pence;
         std::cin.ignore();
         vault[name] = Purse(pounds, shillings, pence);
     }
     Purse total;
-    std::cout << "\nAccount List:\n";
+    std::cout << "\nAccount List\n";
+    std::cout << "============\n";
     for (const auto& [name, purse] : vault) {
-        std::cout << name << ": " << purse << '\n';
+        std::cout << "      " << name << " with " << purse << '\n';
         total += purse;
     }
-    std::cout << "\nTotal amount of money in the bank: " << total << '\n';
+    std::cout << "\nTotal in bank is " << total << '\n';
     return 0;
 }
